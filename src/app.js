1f7c931072ef85e4ead61dd5c74ca7fbf1e6b654
Igor Berlenko <sevenflash@yandex.ru>
2019-03-01 22:41:19 +0300

    implement pageviews endpoint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import visitorsFactory from './visitors'
 
 const app = express()
+const visitors = visitorsFactory()
+
+const ONE_MINUTE = 60 * 1000
 
 app.get('/', (req, res) => {
-    res.json({ now: new Date().toISOString() })
+    visitors.push()
+
+    const now = Number(new Date())
+    const visitorsInLastMinute = visitors.filterByPeriod(now - ONE_MINUTE, now)
+    const pageviews = visitorsInLastMinute.length
+
+    res.json({ pageviews })
 })
 
 app.listen(3000, () => {
