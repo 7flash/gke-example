8cf80f1c59dcae7beb04f237014e5eceeab40ecc
Igor Berlenko <sevenflash@yandex.ru>
2019-03-01 23:04:45 +0300

    add handlebars layout

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
+import path from 'path'
 import express from 'express'
+import handlebars from 'express-handlebars'
 import visitorsFactory from './visitors'
+
+const ONE_MINUTE = 60 * 1000
 
 const app = express()
 const visitors = visitorsFactory()
 
-const ONE_MINUTE = 60 * 1000
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.set('views', path.resolve(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     visitors.push()
 
     const now = Number(new Date())
+
     const visitorsInLastMinute = visitors.filterByPeriod(now - ONE_MINUTE, now)
     const pageviews = visitorsInLastMinute.length
 
-    res.json({ pageviews })
+    res.render('pageviews', { pageviews })
 })
 
 app.listen(3000, () => {
